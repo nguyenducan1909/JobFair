ef50d9c5f66be27cb935b7cacc546525839140db
unknown <04.12t1.nguyenducancvt@gmail.com>
2025-07-31 18:45:25 +0700

    Create project

---
+++ src/main/java/com/nda/jobfair/JobFairApplication.java
@@ -0,0 +1,13 @@
+package com.nda.jobfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nda/jobfair/JobFairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nda.jobfair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
